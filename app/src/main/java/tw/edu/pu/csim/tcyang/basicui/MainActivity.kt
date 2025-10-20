b3bc47702f978075f4325b4ee27d83238aba3e18
Brian Shih <shih.brian123@gmail.com>
2025-10-20 15:44:29 +0800

    v3: button on click changes flag

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -1,29 +1,43 @@
 package tw.edu.pu.csim.tcyang.basicui
 
 
+import android.app.Activity
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
@@ -129,7 +143,51 @@ fun Main(modifier: Modifier = Modifier) {
 
         }
 
+        Spacer(modifier = Modifier.size(10.dp))
 
+        Row{
+
+            Button(onClick = {
+
+            }) {
+
+                Text(text = "歡迎修課")
+
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(onClick = {
+
+            }) {
+
+                Text(text = "展翅飛翔")
+
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(onClick = {
+
+            }) {
+
+                Text(text = "結束App")
+
+            }
+
+        }
+
+        var flag by remember { mutableStateOf(false) }
+
+        Button(onClick = {
+            flag = true
+        }) {
+            if (flag) {
+                Text("test: button clicked!")
+            } else {
+                Text("button")
+            }
+        }
 
     }
 }
